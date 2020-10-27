55ae5d169583e385ad2305df59c63a2525021d69
Tilda Udufo <mathildaudufo@gmail.com>
2020-10-27 18:57:19 +0100

    Removed domain option from production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "heroku.com",
-    authentication: "plain",
+    authentication: "login",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
