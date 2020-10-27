4fbe151e496b60170fa4801f521ef9a40df0664c
Tilda Udufo <mathildaudufo@gmail.com>
2020-10-27 18:23:13 +0100

    Minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host : 'heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'heroku.com' }
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
